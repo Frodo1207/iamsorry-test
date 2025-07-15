7072e7a0112fd5f4e423c33509df08087138a9a4
Frodo1207 <1302105335@qq.com>
2025-07-15 19:07:59 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        view: resolve(__dirname, 'view.html'),
         gallery: resolve(__dirname, 'gallery.html'),
+        view: resolve(__dirname, 'view.html')
       }
     }
   }
